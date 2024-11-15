91c80d3f24dbec98e5d00df66ead8b5ea51b22bb
Ankit <Ankiterram@gmail.com>
2024-11-15 00:58:15 -0500

    added typing effect for welcome section and added soft skills to skills section

--- script.js
+++ script.js
@@ -8,3 +8,38 @@ function changeNavbarColor() {
 }
 
 window.addEventListener("scroll", changeNavbarColor);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const typedMessage = document.getElementById('typed-message');
+    const staticText = "I'm Ankit and I'm a ";
+    const roles = ["Full-Stack Web Developer", "Software Developer"];
+    let roleIndex = 0;
+    let index = 0;
+    let isDeleting = false;
+    let speed = 100;
+
+    function type() {
+        const currentRole = roles[roleIndex];
+        const currentText = staticText + currentRole.substring(0, index);
+        typedMessage.innerHTML = currentText;
+
+        if (!isDeleting && index < currentRole.length) {
+            index++;
+            speed = 80;
+        } else if (isDeleting && index > 0) {
+            index--;
+            speed = 20;
+        } else if (!isDeleting && index === currentRole.length) {
+            isDeleting = true;
+            // Pause before deleting
+            speed = 2000; 
+        } else if (isDeleting && index === 0) {
+            isDeleting = false;
+            roleIndex = (roleIndex + 1) % roles.length;
+            // Pause before typing again
+            speed = 500; 
+        }
+        setTimeout(type, speed);
+    }
+    type();
+});
